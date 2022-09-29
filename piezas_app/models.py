35301a9127dfe8e41016f8425eb5504dca82129a
Jose Ernesto Carreno Bueno <josernesto1989@gmail.com>
2022-09-29 11:59:53 -0400

    adding models

--- piezas_app/models.py
+++ piezas_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Modelo(models.Model):
+    name = models.CharField(max_length=255)
+    
+
+class Marca(models.Model):
+    name = models.CharField(max_length=255)
+
+class TipoPieza(models.Model):
+    name = models.CharField(max_length=255)
+    
+    
+class Taller(models.Model):
+    name = models.CharField(max_length=255)
+    location = models.CharField(max_length=255)
+    
